a785043e0c47f3478b9d182c82683e5c69a0c4fa
yacut <roman.rogozhnikov@gmail.com>
2017-11-19 21:12:11 +0100

    add log

--- kubernetes-rbac-synchroniser.go
+++ kubernetes-rbac-synchroniser.go
@@ -13,5 +13,6 @@ var addr = flag.String("listen-address", ":8080", "The address to listen on for 
 func main() {
 	flag.Parse()
 	http.Handle("/metrics", promhttp.Handler())
+	log.Println("Server listing on port 8080...")
 	log.Fatal(http.ListenAndServe(*addr, nil))
 }
